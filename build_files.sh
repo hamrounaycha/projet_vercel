b508974ab9f94360b30c79740f1204c2e469e436
hamrounaycha <hamrounaycha@gmail.com>
2023-01-01 22:49:08 +0100

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,3 @@
 # build_files.sh
-
-python3.9 -m pip install -r requirements.txt
-
-
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
-
-
-
-python3.9 manage.py collectstatic --noinput --clear
+pip install -r requirements.txt
+python3.9 manage.py collectstatic
