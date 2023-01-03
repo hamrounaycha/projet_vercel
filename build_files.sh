4fe68470a5839c103b6562c44293dc26caaf173b
hamrounaycha <hamrounaycha@gmail.com>
2023-01-03 16:43:27 +0100

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 # build_files.sh
 echo "Building the project ..."
-pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
 echo "Make Static ..."
 python manage.py collectstatic
