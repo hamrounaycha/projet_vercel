7fe5336159402002ed93eff07a1dae91577a20e8
hamrounaycha <hamrounaycha@gmail.com>
2023-01-04 11:38:23 +0100

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,12 +1,9 @@
 # build_files.sh
 echo "Building the project ..."
 python -m pip install -r requirements.txt
-echo "Make Migrations ..."
-python manage.py makemigrations 
-python manage.py migrate 
-echo "Make Static ..."
-python manage.py collectstatic 
 
+echo "Make Static ..."
+python manage.py collectstatic --noinput –clear
 
 
 
